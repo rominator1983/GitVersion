6da4dabc236ddcb6f75c0782f39596cbfe9482fb
Artur <arturcic@gmail.com>
2022-05-07 18:11:52 +0200

    applied null check fixes from main

--- src/GitVersion.LibGit2Sharp/Git/RefSpec.cs
+++ src/GitVersion.LibGit2Sharp/Git/RefSpec.cs
@@ -9,9 +9,10 @@ public class RefSpec : IRefSpec
     private static readonly LambdaKeyComparer<IRefSpec, string> comparerHelper = new(x => x.Specification);
     private readonly LibGit2Sharp.RefSpec innerRefSpec;
 
-    internal RefSpec(LibGit2Sharp.RefSpec refSpec) => this.innerRefSpec = refSpec.NotNull();
-    public int CompareTo(IRefSpec other) => comparerHelper.Compare(this, other);
-    public bool Equals(IRefSpec? other) => equalityHelper.Equals(this, other);
+    internal RefSpec(LibGit2Sharp.RefSpec refSpec)
+        => this.innerRefSpec = refSpec.NotNull();
+    public int CompareTo(IRefSpec? other) => ComparerHelper.Compare(this, other);
+    public bool Equals(IRefSpec? other) => EqualityHelper.Equals(this, other);
     public string Specification => this.innerRefSpec.Specification;
     public RefSpecDirection Direction => (RefSpecDirection)this.innerRefSpec.Direction;
     public string Source => this.innerRefSpec.Source;
